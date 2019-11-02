6f95f731aceb48c64d548fd2e7eae2653c348020
github_ajacin <arunjacob2@gmail.com>
2019-11-02 15:11:35 +0530

    enabled splashscreen logo

--- lib/tabs/BasicInfoScreen.dart
+++ lib/tabs/BasicInfoScreen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 
-class BasicInfoScreen extends StatelessWidget {
-  _printToast(String message){
+class BasicInfoScreen extends StatefulWidget {
+  @override
+  _BasicInfoScreenState createState() => new _BasicInfoScreenState();
+}
+
+class _BasicInfoScreenState extends State<BasicInfoScreen> {
+  double _avatarWidth = 180;
+  double _childNameFontSize=20;
+  bool _isDetailsVisible = true;
+
+  _printToast(String message) {
     Fluttertoast.showToast(
         msg: "$message",
         toastLength: Toast.LENGTH_SHORT,
@@ -10,9 +21,19 @@ class BasicInfoScreen extends StatelessWidget {
         timeInSecForIos: 1,
         backgroundColor: Colors.black,
         textColor: Colors.white,
-        fontSize: 10.0
-    );
-  } 
+        fontSize: 10.0);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 10), () {
+      setState(() {
+        _avatarWidth = 60;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,23 +45,64 @@ class BasicInfoScreen extends StatelessWidget {
             children: <Widget>[
               Container(
                 padding: new EdgeInsets.symmetric(vertical: 10.0),
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://lh3.googleusercontent.com/tVRP409qAIh9vsuq213HESrrHIyAA5Zb5WkX_5WxqA8wgix7oknjSsu6jYiYxO0yDJo7sVvIhW0Pg4Zsr1OWIpSaLlbgeqVZqqFWBGV9WtRfO52rAlUx07x2s-DxWfTMDDKLBtzInRQ=w1454-h969-no'),
-                radius: 100,
-                backgroundColor: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    CircularProfileAvatar(
+                      'https://lh3.googleusercontent.com/tVRP409qAIh9vsuq213HESrrHIyAA5Zb5WkX_5WxqA8wgix7oknjSsu6jYiYxO0yDJo7sVvIhW0Pg4Zsr1OWIpSaLlbgeqVZqqFWBGV9WtRfO52rAlUx07x2s-DxWfTMDDKLBtzInRQ=w1454-h969-no', //sets image path, it should be a URL string. default value is empty string, if path is empty it will display only initials
+                      radius: _avatarWidth,
+                      backgroundColor: Colors.transparent,
+                      borderWidth: 4,
+                      initialsText: Text(
+                        "ADAM",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      borderColor: Colors.white,
+                      elevation: 8.0,
+                      foregroundColor: Colors.white.withOpacity(0.1),
+                      cacheImage: true,
+                      onTap: () {
+                        setState(() {
+                          _avatarWidth = (_avatarWidth == 180)
+                              ? _avatarWidth = 60
+                              : _avatarWidth = 180;
+                          _childNameFontSize = (_childNameFontSize == 20)
+                              ? _childNameFontSize = 40
+                              : _childNameFontSize = 20;
+                          _isDetailsVisible =!_isDetailsVisible;
+                        });
+                      },
+                      showInitialTextAbovePicture: false,
+                    ),
+                    Padding(
+  padding: EdgeInsets.only(top:10.0),
+  child: Text(
+                      "Adam Jacob",
+                      style: TextStyle(
+                          fontSize: _childNameFontSize,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+),
+                    
+                  ],
+                ),
               ),
-              ),
-              Card(
-                color: Theme.of(context).accentColor,
-                child: ListView(
-                shrinkWrap: true,
+              Visibility(
+                visible:_isDetailsVisible,
+                child:
+              Column(
                 children: <Widget>[
+                  Card(
+                color: Theme.of(context).accentColor,
+                elevation: 8,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
                     ListTile(
                       leading: Icon(
                         Icons.cake,
                         size: 40,
-                    color: Theme.of(context).primaryColor,
+                        color: Theme.of(context).primaryColor,
                       ),
                       title: Text('January 27, 2018'),
                       subtitle: Text('Date of birth'),
@@ -49,7 +111,7 @@ class BasicInfoScreen extends StatelessWidget {
                       leading: Icon(
                         Icons.local_hospital,
                         size: 40,
-                    color: Theme.of(context).primaryColor,
+                        color: Theme.of(context).primaryColor,
                       ),
                       title: Text('Baby Memorial Hospital, Calicut'),
                       subtitle: Text('Place of birth'),
@@ -58,16 +120,17 @@ class BasicInfoScreen extends StatelessWidget {
                       leading: Icon(
                         Icons.person,
                         size: 40,
-                    color: Theme.of(context).primaryColor,
+                        color: Theme.of(context).primaryColor,
                       ),
                       title: Text('Indian'),
                       subtitle: Text('Nationality'),
                     ),
-                ],
-              ),
+                  ],
+                ),
               ),
               Card(
                   color: Theme.of(context).accentColor,
+                  elevation: 8.0,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
@@ -121,6 +184,9 @@ class BasicInfoScreen extends StatelessWidget {
                       ),
                     ],
                   )),
+                ],
+              ),
+          ),
             ],
           ),
         ),
